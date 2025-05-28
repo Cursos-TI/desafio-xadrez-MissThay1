2906b0cac02631edf477e668e71e82af7c8531de
Tatah <thaysamoura0001@gmail.com>
2025-05-28 21:43:29 +0000

    Testando as printf para a introdução do jogo.

---
+++ Xadrez.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero;
+
+    
+
+ printf("Bem-vindo(a) jogador de xadrez!\n");
+ printf("Você pode escolher uma peça para jogar!(Você é o jogador das peças brancas)\n");
+ printf("QUal peça você escolhe?");
+
+ printf("Escolha uma opção:\n");
+    printf("1 - Torre\n");
+    printf("2 - Bispo\n");
+    printf("3 - Rainha\n");
+
+
+    
+
+ 
+ return 0;
+
+
+}

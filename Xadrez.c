bcb29423c00405f2e4ce6c8ea4f28d32a176f261
Tatah <thaysamoura0001@gmail.com>
2025-05-29 02:24:19 +0000

    Comecei a desenvolver o catalogo para escolher peças, movimentos e jogadas  comecei pela torre, e estou testando para ver como vou fazer ela se mover  a quantas casas o jogador vai querer, mas nao sei se o comando esta certo, vou  fazer mais alterações se for necessário.

--- Xadrez.c
+++ Xadrez.c
@@ -1,25 +1,51 @@
 #include <stdio.h>
 
-int main(){
-
+int main() {
     int numero;
 
-    
+    printf("Bem-vindo(a) jogador de xadrez, escolha uma peça para jogar!\n");
+    printf("Você é o jogador das peças brancas.\n");
+    printf("Qual peça você escolhe?\n");
 
- printf("Bem-vindo(a) jogador de xadrez!\n");
- printf("Você pode escolher uma peça para jogar!(Você é o jogador das peças brancas)\n");
- printf("QUal peça você escolhe?");
-
- printf("Escolha uma opção:\n");
+    printf("Escolha uma opção:\n");
     printf("1 - Torre\n");
     printf("2 - Bispo\n");
     printf("3 - Rainha\n");
-
-
     
+    scanf("%d", &numero);
 
- 
- return 0;
+    switch (numero) {
+        case 1: {
+            printf("Você escolheu a Torre!\n");
+            printf("A Torre se move em linha reta, tanto na horizontal quanto na vertical.\n");
+            printf("Em qual direção você deseja mover a Torre?\n");
+            printf("1 - Horizontal\n");
+            printf("2 - Vertical\n");
 
+            int direcao;
+            scanf("%d", &direcao);
 
+            if (direcao == 1) {
+                printf("Você moveu a Torre horizontalmente.\n");
+            } else if (direcao == 2) {
+                printf("Você moveu a Torre verticalmente.\n");
+            } else {
+                printf("Direção inválida!\n");
+            }
+            break;
+        }
+        case 2:
+            printf("Você escolheu o Bispo!\n");
+            printf("O Bispo se move pelas diagonais do tabuleiro.\n");
+            break;
+        case 3:
+            printf("Você escolheu a Rainha!\n");
+            printf("A Rainha é a peça mais poderosa, pois ela se move em qualquer direção: horizontal, vertical ou diagonal.\n");
+            break;
+        default:
+            printf("Opção inválida!\n");
+            break;
+    }
+          
+    return 0;
 }
